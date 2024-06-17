c7b9ef7c3ffea1e1014ad6401ca708dbd630cb20
PedroMaroal <pedro.maroal@gmail.com>
2024-06-17 18:43:03 +0100

    prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
-              width={100}
+              width={150}
               height={24}
               priority
             />
